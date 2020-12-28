f5fbba4ac8a0b667681c9fcb1aa697c97772e47e
olelit <litasov00@gmail.com>
2020-12-28 22:40:52 +0300

    Complete some tasks

---
+++ 2DArrays/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    static bool inMatrix(int[][] arr, int x, int y)
+    {
+        int[] leftTop = { 0, 0 };
+        int[] rightBottom = { arr.GetLength(0) - 1, arr[0].Length - 1 };
+
+        if(x > leftTop[0] && x < rightBottom[0] && y > leftTop[1] && y < rightBottom[1])
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    static int summSmallMatrix(int[][] arr, int x, int y)
+    {
+        int sum = 0;
+        int[][] smallMatrix =
+        {
+            new int[3] { arr[y-1][x-1], arr[y-1][x], arr[y-1][x+1] },
+            new int[1] { arr[y][x] },
+            new int[3] { arr[y+1][x-1], arr[y+1][x], arr[y+1][x+1] },
+        };
+        foreach (int[] row in smallMatrix)
+        {
+            foreach(int col in row)
+            {
+                sum += col;
+            }
+        }
+        return sum;
+    }
+
+    // Complete the hourglassSum function below.
+    static int hourglassSum(int[][] arr)
+    {
+        int result = 0;
+
+        for (int y = 1; y < arr.GetLength(0); y++)
+        {
+            for (int x = 1; x < arr[y].Length; x++)
+            {
+                if (inMatrix(arr, x, y))
+                {
+                    int sum = summSmallMatrix(arr, x, y);
+                    if (sum > result || (x==1 && y == 1))
+                    {
+                        result = sum;
+                    }
+
+                    
+                }
+            }
+        }
+        return result;
+
+    }
+
+    static void Main(string[] args)
+    {
+        int[][] arr = new int[6][] 
+        { 
+            new int[6] { -1,1,-1,0,0,0}, 
+            new int[6] { 0,-1,0,0,0,0}, 
+            new int[6] { -1,-1,-1,0,0,0}, 
+            new int[6] { 0,-9,2,-4,-4,0}, 
+            new int[6] { -7,0,0,-2,0,0}, 
+            new int[6] { 0,0,-1,-2,-4, 0}, 
+        };
+        int result = hourglassSum(arr);
+        Console.WriteLine(result);
+    }
+}
---
+++ ArraysLeftRotation/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the rotLeft function below.
+    static int[] rotLeft(int[] a, int d)
+    {
+        int i = 0;
+        int[] result = new int[a.Length];
+        while (i < d)
+        {
+            i++;
+            int[] elem = new int[] { a[0] };
+            Array.Copy(a,1,result, 0, a.Length-1);
+            Array.Copy(elem,0, result, a.Length - 1, elem.Length);
+            a = result;
+        }
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        string path = Path.GetFullPath(@"..\..\..\");
+
+        string[] lines = File.ReadAllLines(path + "data.txt");
+        string[] nd = lines[0].Split();
+
+        int n = Convert.ToInt32(nd[0]);
+
+        int d = Convert.ToInt32(nd[1]);
+        int i = 0;
+        string[] data = lines[1].Split();
+        int[] a = new int[data.Length];
+        foreach (string item in data)
+        {
+            a[i] = int.Parse(item);
+            i++;
+        }
+        DateTime start = DateTime.Now;
+        int[] result = rotLeft(a, d);
+        DateTime end = DateTime.Now;
+        Console.WriteLine(string.Join(" ", result));
+        Console.WriteLine(end - start);
+    }
+
+
+}
